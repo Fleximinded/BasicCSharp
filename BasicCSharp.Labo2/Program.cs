bdc287a9adf3758acb4b8063fa3fb7103046ba11
Filip Geens <filip.geens@fleximinded.com>
2023-09-27 22:13:06 +0200

    Labo 2 made

--- BasicCSharp.Labo2/Program.cs
+++ BasicCSharp.Labo2/Program.cs
@@ -4,6 +4,23 @@
     {
         static void Main(string[] args)
         {
+            float Pi = 3.14f;
+            Console.Write("Pi = ");
+            Console.WriteLine(Pi);
+            // Oef 2
+            decimal getal = 2500.25M;
+            Console.Write("getal = ");
+            Console.WriteLine(getal);
+            // Oef 3
+            int maxInt = int.MaxValue;
+            Console.Write("maxInt = ");
+            Console.WriteLine(maxInt);
+            Console.Write("maxInt + 1 = "); 
+            Console.WriteLine(++maxInt);
+            // Oef 4
+            ushort maxUShort = ushort.MaxValue;
+            Console.Write("maxUShort = ");  
+            Console.WriteLine(++maxUShort);
         }
     }
 }
