9c270d27b456b6632cc657b6aaec51291df927cf
Filip Geens <filip.geens@fleximinded.com>
2023-10-02 21:07:13 +0200

    dummy added

---
+++ BasicCSharp.Labo2/dummy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCSharp.Labo2
+{
+    public class Dummy
+    {
+    }
+}
