6cd56e32c52058348004d4c9a2e024b788ffb6d6
Filip Geens <filip.geens@fleximinded.com>
2023-10-18 20:48:25 +0200

    Labo 1 - Versie 1

---
+++ BasicCSharp.Labos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace BasicCSharp.Labos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+            while(isRunning)
+            {
+                Console.Write(">");
+                string input = Console.ReadLine() ?? "";
+                switch(input.ToLower())
+                {
+                    case "exit":
+                    case "quit":
+                        isRunning = false;
+                        break;
+                    case "echo":
+                        Console.WriteLine($"You wrote '{input}' ");
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command");
+                        break;
+                }
+            }
+        }
+    }
+}
