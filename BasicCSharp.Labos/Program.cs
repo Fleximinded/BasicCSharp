8657fba8b68585e6e28964324f21718c1a574688
Filip Geens <filip.geens@fleximinded.com>
2023-10-23 21:48:52 +0200

    Eerste deel labo 2

--- BasicCSharp.Labos/Program.cs
+++ BasicCSharp.Labos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Drawing;
 
 namespace BasicCSharp.Labos
 {
@@ -7,9 +8,12 @@ namespace BasicCSharp.Labos
         static void Main(string[] args)
         {
             bool isRunning = true;
+            string currentCursor = ">";
+            ConsoleColor currentCursorColor = ConsoleColor.Green;
+            ConsoleColor backgroundColor = ConsoleColor.Black;
             while(isRunning)
             {
-                Console.Write(">");
+                ShowCursor(currentCursor, currentCursorColor);
                 string input = Console.ReadLine() ?? "";
                 switch(input.ToLower())
                 {
@@ -17,14 +21,65 @@ namespace BasicCSharp.Labos
                     case "quit":
                         isRunning = false;
                         break;
+                    case "clear":
+                    case "cls":
+                        Console.Clear();
+                        break;
                     case "echo":
                         Console.WriteLine($"You wrote '{input}' ");
                         break;
+                    case "setcursor":
+                        currentCursor = SetCursor(currentCursor);
+                        break;
+                        case "setcursorcolor":
+                            currentCursorColor=SetCursorColor(currentCursorColor,backgroundColor);
+                        break;
                     default:
-                        Console.WriteLine("Unknown command");
+                        ShowError($"The command '{input}' is not known in my wonderful system", ConsoleColor.DarkMagenta);
                         break;
                 }
             }
+        }
+
+        private static ConsoleColor SetCursorColor(ConsoleColor currentCursorColor, ConsoleColor backgroundColor)
+        {
+            Console.Write("Enter the new cursor color: ");
+            string color = Console.ReadLine() ?? "";
+            if(string.IsNullOrEmpty(color)!=true)
+            {
+                if(Enum.TryParse<ConsoleColor>(color, true,out ConsoleColor newColor)) {
+                    return newColor;
+                }
+            }
+            ShowError($"The value '{color}' is not a valid color");
+            return currentCursorColor;
+        }
+
+        private static void ShowCursor(string currentCursor, ConsoleColor currentCursorColor)
+        {
+            Console.ForegroundColor = currentCursorColor;
+            Console.Write(currentCursor);
+            Console.ResetColor();
+        }
+
+        static void ShowError(string message,ConsoleColor errorColor= ConsoleColor.Red)
+        {
+            ConsoleColor oldColor=Console.ForegroundColor;
+            Console.ForegroundColor = errorColor;
+            Console.WriteLine(message);
+            Console.ForegroundColor = oldColor;
+        }
+        static string SetCursor(string currentCursor)
+        {
+            Console.Write("Enter the new cursor: ");
+            string cursor = Console.ReadLine() ?? "";
+            if(string.IsNullOrEmpty(cursor) || char.IsLetterOrDigit(cursor, cursor.Length - 1))
+            {
+                ShowError($"The cursor '{cursor}' is not valid. The last character may not be a number or letter");
+                return currentCursor;
+            }
+            Console.Clear();
+            return cursor;
         }
     }
 }
